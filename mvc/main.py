5cb4a0bed633983015cf0625d352b5c3826ab80f
yrwq <yrwqid@gmail.com>
2021-01-26 09:10:48 +0100

    update

--- mvc/main.py
+++ mvc/main.py
@@ -2,6 +2,10 @@
 import search
 import sys
 import getopt
+import inquirer
+from pprint import pprint
+
+# pip install inquirer
 
 
 def help():
@@ -14,9 +18,40 @@ Usage:
 """)
 
 
+def print_search(query):
+    movie = search.search_movie(query)
+    movie_title = movie[0]
+    movie_plot = movie[1]
+    movie_genre = movie[2]
+
+    if movie[2][1]:
+        print(f"\n\n{movie_title} - {movie_genre[0]}, {movie_genre[1]}")
+    else:
+        print(f"\n\n{movie_title} - {movie_genre[0]}")
+    print(f"\n\n{movie_plot[0]}\n\n")
+    if movie_plot[1]:
+        print(f"{movie_plot[1]}\n\n")
+
+
+def choose_from_top():
+    top = search.get_top()
+
+    movies = [
+        inquirer.List(
+            "result",
+            message="Choose a movie:",
+            choices=top,
+        ),
+    ]
+
+    title = inquirer.prompt(movies)
+    query = str(title["result"])
+    print_search(query)
+
+
 def main():
     try:
-        options, value = getopt.getopt(sys.argv[1:], 'hs:', ['help', 'search'])
+        options, value = getopt.getopt(sys.argv[1:], 'hs:t', ["help", "search", "top"])
 
         for opt, arg in options:
 
@@ -24,22 +59,13 @@ def main():
                 help()
 
             elif opt in ("-s" or "--search"):
-                movie = search.search_movie(arg)
-                movie_title = movie[0]
-                movie_plot = movie[1]
+                print_search(arg)
 
-
-                print(f"\n\n{movie_title}")
-                print(f"\n\n{movie_plot[0]}\n\n")
-                if movie_plot[1]:
-                    print(f"{movie_plot[1]}\n\n")
-
-                # movie_actors = movie[2]
-                # for actor in movie_actors:
-                #     print(actor, end=", ")
+            elif opt in ("-t" or "--top"):
+                choose_from_top()
 
             else:
-                print("Help (ELSE)")
+                help()
 
     except getopt.error:
         help()
--- mvc/search.py
+++ mvc/search.py
@@ -13,10 +13,17 @@ def search_movie(name):
     search = ia.search_movie(name)
     movie = ia.get_movie(search[0].movieID)
 
+
     movie_data = []
 
     movie_data.append(movie["title"])
     movie_data.append(movie["plot"])
-    movie_data.append(movie["cast"])
+    movie_data.append(movie["genres"])
 
     return movie_data
+
+
+def get_top():
+    top = ia.get_top250_movies()
+
+    return top
